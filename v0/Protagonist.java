aa54b2d3068a4f1295537fbbad781c92d15642e3
Jason Zhou <jzhou33@stuy.edu>
2021-11-22 14:06:00 -0500

    e

--- v0/Protagonist.java
+++ v0/Protagonist.java
@@ -1,58 +1,14 @@
-public class Protagonist{
+public class Protagonist extends Character{
 
 
 
-
-  private int health;
-  private int maxHealth;
-  private int damage;
-  private String attackState = "";
-  private String protagonistName = "";
-  private int level;
-
-  public Being() {
-    this.health = 100;
-    this.damage = 15;
-    this.level = 0;
-    this.attackState = "normalize";
+  public Protagonist() {
+    super();
   }
-  public Being(String name) {
-    this();
-    this.protagonistName = name;
-  }
-  public String getName() {
-    return this.protagonistName;
-  }
-  public int getHealth() {
-    return this.health;
+  public Protagonist(String name) {
+    super(name);
   }
 
-  public String toString() {
-    return "Name: " + getName() + "\nLevel: " + this.level + "\nHealth: " + this.health + "\nAttack State" + this.attackState;
-  }
-  public void specialize() {
-    this.attackState = "specialize";
-  }
-  public void normalize() {
-    this.attackState = "normalize";
-  }
-  public boolean isAlive() {
-    return health >= 0;
-  }
-  public void takeDamage(int damage) {
-    this.health -= damage;
-  }
-  public void increaseLevel(int health, int damage) {
-    this.maxHealth += health;
-    this.damage += damage;
-    this.level++;
-  }
-  public void resetHealth() {
-    this.health = maxHealth;
-  }
-  public int attack() {
-    return 0;
-  }
   public int attack(Monster name) {
     if (attackState.equals("specialize")) {
       //System.out.println("Swinging Hammer...");
