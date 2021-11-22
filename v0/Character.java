9af1f357767d8c1bab630ac40449b8533771539f
independence106 <independence106@gmail.com>
2021-11-22 14:17:11 -0500

    Update Character.java

--- v0/Character.java
+++ v0/Character.java
@@ -1,7 +1,11 @@
 public class Character {
+  //attributes
   protected int health;
   protected int maxHealth;
-  protected int damage;
+  protected double damage;
+  protected int strength;
+  protected double attackRating;
+  protected int defense;
   protected String attackState = "";
   protected String name = "";
   protected int level;
@@ -17,13 +21,16 @@ public class Character {
     this();
     this.name = name;
   }
+  
   public String getName() {
     return this.name;
   }
   public int getHealth() {
     return this.health;
   }
-
+  public void getDefense() [
+    return this.defense; 
+  }
   public String toString() {
     return "Name: " + getName() + "\nLevel: " + this.level + "\nHealth: " + this.health + "\nAttack State" + this.attackState;
   }
@@ -33,11 +40,17 @@ public class Character {
   public void normalize() {
     this.attackState = "normalize";
   }
+  public void lowerHP(int amount) {
+    this.health -= amount;
+    
   public boolean isAlive() {
     return health >= 0;
   }
   public void takeDamage(int damage) {
     this.health -= damage;
+  }
+  public void calcNewDamage(Character char) {
+    this.damage = (this.strenth * this.attackRating) - char.getDefense();
   }
   public void increaseLevel(int health, int damage) {
     this.maxHealth += health;
