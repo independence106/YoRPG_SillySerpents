e436bbfcc875b7ef1d3541636c828d3476309c2a
Jason Zhou <jzhou33@stuy.edu>
2021-11-24 14:04:35 -0500

    e

---
+++ v4/Sniper.java
@@ -0,0 +1,11 @@
+/**
+ * Sniper: One attack per 2 rounds
+ *
+ * Special: Head-shot - 30% chance to instakill mobs but not boss. 1% chance to 1 shot boss.
+ */
+public class Sniper extends Archer {
+  public Sniper() {
+    super();
+    
+  }
+}
